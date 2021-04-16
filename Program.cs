b1e1248e0f34909eb534da60a5b7605b14d7372b
Diego Neri <diego@diegoneri.com>
2021-04-16 20:51:48 -0300

    Finaliza o exercício PesoIdeal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace PesoIdeal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double altura, pesoIdeal;
+            string sexo;
+
+            Console.Write("Digite sua altura em m..........: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Sexo [M]asculino / [F]eminino...: ");
+            sexo = Console.ReadLine();
+
+            if (sexo == "M")
+            {
+                pesoIdeal = altura * 72.7 - 58.0;
+                Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg.");
+            }
+            else if (sexo == "F")
+            {
+                pesoIdeal = altura * 62.1 - 44.7;
+                Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg.");
+            }
+            else
+            {
+                Console.WriteLine($"O sexo informado [{sexo}] é inválido");
+            }
+
+
+
         }
     }
 }
